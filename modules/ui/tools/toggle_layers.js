3111c41103a91ec8c9cf0c44cacc499ffd1160dd
andrewcp54 <andrewcp540@gmail.com>
2020-06-11 10:53:52 -0600

    toolbar btns actually toggle layer now

--- modules/ui/tools/toggle_layers.js
+++ modules/ui/tools/toggle_layers.js
@@ -8,8 +8,35 @@ import {
 import { t, localizer } from '../../core/localizer';
 import { svgIcon } from '../../svg';
 import { uiTooltip } from '../tooltip';
-import { presetManager } from '../../presets';
+import { modeBrowse } from '../../modes/browse';
 
+
+function showsLayer(which) {
+    var layer = context.layers().layer(which);
+    if (layer) {
+        return layer.enabled();
+    }
+    return false;
+}
+
+function setLayer(which, enabled) {
+    // Don't allow layer changes while drawing - #6584
+    var mode = context.mode();
+    if (mode && /^draw/.test(mode.id)) return;
+
+    var layer = context.layers().layer(which);
+    if (layer) {
+        layer.enabled(enabled);
+
+        if (!enabled && (which === 'osm' || which === 'notes')) {
+            context.enter(modeBrowse(context));
+        }
+    }
+}
+
+function toggleLayer(which) {
+    setLayer(which, !showsLayer(which));
+}
 
 export function uiToolToggle(context) {
 
@@ -21,14 +48,14 @@ export function uiToolToggle(context) {
     var layers = [
         {
             title: 'OSM Layers',
-            id: 'toggle-osm',
+            id: 'osm',
             button: 'line',
             description: 'Toggle OSM Features On/Off',
             key: '⌥W'
         },
         {
             title: 'Prop Layers',
-            id: 'toggle-prop',
+            id: 'prop-features',
             button: 'line',
             description: 'Toggle Proprietary Features On/Off',
             key: '⌥Q'
@@ -64,8 +91,8 @@ export function uiToolToggle(context) {
             var buttonsEnter = buttons.enter()
                 .append('button')
                 .attr('class', function(d) { return d.id + ' add-button bar-button'; })
-                .on('click.mode-buttons', function() {
-                    console.log('Hello There!');
+                .on('click.mode-buttons', function(d) {
+                    toggleLayer(d.id);
                 })
                 .call(uiTooltip()
                     .placement('bottom')
